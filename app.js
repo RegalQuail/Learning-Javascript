02402f61d2e090e84b12b1b163e435901da2abfb
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-12-29 13:17:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("Hello World");
 
 var numbers = [2, 5, 7, 4, 8, 1, 3, 0, 6, 9];
 
-console.log(numbers.sort());
+console.log("Here's the original list: " + numbers);
+console.log("Here's the same list, sorted: " + numbers.sort());
