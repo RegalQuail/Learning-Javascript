f8dbb01f40b48aac650913b7d95cb05ff01ae3e3
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2022-02-02 14:15:51 +0100

    more js on web

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+const numbers = [2, 5, 7, 4, 8, 1, 3, 0, 6, 9];
+document.getElementById('numbers').innerHTML = "List: " + numbers;
+
+const sorted = numbers.sort();
+document.getElementById('sorted').innerHTML = "Sorted list: " + sorted;
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let test = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+let matrix = {};
+
+document.getElementById('array').innerHTML = array;
+document.getElementById('test').innerHTML = test;
+document.getElementById('addArrays').innerHTML = array + ' + ' + test;
+document.getElementById('matrix').innerHTML = matrix;
+
